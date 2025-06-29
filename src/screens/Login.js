021562294674b079efc0f739807afc8829ac03cd
sajida-dev <sajidajaved604@gmail.com>
2025-06-29 10:39:47 +0500

    input update

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
+import { useState } from 'react';
+
 
 export default function Login() {
+    const [Email, setEmail] = useState()
     return (
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Text>{Email ?? "No email"}</Text>
+            <TextInput placeholder='Enter Your Email' onChangeText={setEmail} style={styles.input} />
         </View>
     )
 }
-const styles = StyleSheet.create({
 
-})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 20
+    },
+    input: {
+        borderWidth: 2,
+        width: 200
+    }
+});
